c1c45361e6fa3d6949a1e727894d90febb0818db
Gabriel Oliveira <bielaugustooliveira@hotmail.com>
2026-05-21 08:39:44 -0300

    update init

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -8,9 +8,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['UPLOAD_FOLDER'] = os.getenv('UPLOAD_FOLDER')
+app.config['UPLOAD_FOLDER'] = "static/imagens"
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
